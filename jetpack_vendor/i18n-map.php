b757e2e47ccb1b802f0c78a83b3cb8add64105d6
zaerl <zaerl@users.noreply.github.com>
2024-05-09 10:04:30 +0000

    Changelog and readme.txt edits. (#37305)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9015506601
    
    Upstream-Ref: Automattic/jetpack@d6482129110a27a9bd11f49be8e2b4fc41f36632

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.1.0-alpha1714758062',
+      'ver' => '0.1.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
