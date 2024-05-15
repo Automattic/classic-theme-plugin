b25cf6eceeee7eb860703040d69402e3822620f9
anomiex <anomiex@users.noreply.github.com>
2024-05-15 16:35:33 +0000

    Update react monorepo to v18.3.1 (#37380)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9099249530
    
    Upstream-Ref: Automattic/jetpack@7f3e5f718176948d5cf8e7352561f0bc49f784ee

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '54830d230514d4990bd8');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2e3482bf163e5c668317');
