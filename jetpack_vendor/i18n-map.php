08a218fa4f115333da2bd68647c296afbc2c6b21
ice9js <ice9js@users.noreply.github.com>
2024-10-08 11:47:44 +0000

    Changelog and readme.txt edits. (#39668)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11234528174
    
    Upstream-Ref: Automattic/jetpack@88842fc23b861907e069de17139e86d5ca42c453

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.9-alpha1727884694',
+      'ver' => '2.3.9',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.6.3-alpha1727884694',
+      'ver' => '0.6.3',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
