a8356a2f72c920c2e08a115ca5d3b30daebb76f9
anomiex <anomiex@users.noreply.github.com>
2024-12-11 16:06:36 +0000

    Update to eslint 9 (#40555)
    Eslint 9 brings a new config format, which means this PR also rewrites
    all the eslint configs. I took the opportunity to clean them up
    (although a good bit of that wound up in earlier PRs), and added code to
    fetch the textdomain from composer.json and whether the project uses
    React from package.json so we don't need to have so many boilerplate
    configs that just set a textdomain or enable React rules.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12280005125
    
    Upstream-Ref: Automattic/jetpack@9e3ae2f2e3140f60ae88df608bc1cbc34d275109

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.1',
+      'ver' => '4.0.2-alpha1733932294',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.8.0-alpha1733921825',
+      'ver' => '0.8.0-alpha1733932294',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
