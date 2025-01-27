b21ac62feabe2c3e6b83305ff74f3ddae7a23cc5
anomiex <anomiex@users.noreply.github.com>
2025-01-27 17:28:03 +0000

    Update dependency webpack-cli to v6 (#41286)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12994416343
    
    Upstream-Ref: Automattic/jetpack@dd5f0a850001b9ef88b8e1f6da486e2fc0a8f15d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.3',
+      'ver' => '4.0.4-alpha1737997918',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.9.2',
+      'ver' => '0.9.3-alpha1737997918',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
