3e55d41f5e65c5f105e0f648e134d0ddf641887f
zinigor <zinigor@users.noreply.github.com>
2025-02-17 14:11:54 +0000

    Changelog and readme.txt edits. (#41847)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13371887680
    
    Upstream-Ref: Automattic/jetpack@a79304117e59322f2fe70ccea4079e62c7058528

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.6',
+      'ver' => '4.0.7',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.10.0',
+      'ver' => '0.10.1',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
