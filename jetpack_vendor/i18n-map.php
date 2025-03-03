31973513a27510e06f742ca973e6dbee8a124232
zinigor <zinigor@users.noreply.github.com>
2025-03-03 16:46:48 +0000

    Update wordpress monorepo (#42163)
    * Update wordpress monorepo
    
    * Added an enhanced mock to the test, h/t @anomiex for the fix!
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Igor Zinovyev <zinigor@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13635659867
    
    Upstream-Ref: Automattic/jetpack@e65723f397e646f0c9807d4751d1d8625e037968

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.8',
+      'ver' => '4.0.9-alpha1741019398',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.11.1-alpha1740763194',
+      'ver' => '0.11.1-alpha1741019398',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
