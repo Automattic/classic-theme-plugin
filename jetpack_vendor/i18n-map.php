9a1a5a31433f5411d0c4061f668b188dfa239a55
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-03-26 13:06:16 +0000

    Changelog and readme.txt edits. (#42717)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14083716464
    
    Upstream-Ref: Automattic/jetpack@85cf2195e4ed5eb9035adecbad1f7a837875487b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '3.1.0-alpha1742846885',
+      'ver' => '3.1.0',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
