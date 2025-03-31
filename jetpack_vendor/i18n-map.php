0b356499fbf944a72751d5422e806047b0fd999d
tbradsha <tbradsha@users.noreply.github.com>
2025-03-31 18:36:33 +0000

    Backport jetpack 14.5-beta Changes (#42800)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14178526665
    
    Upstream-Ref: Automattic/jetpack@d73d447978b0dbb214f9d9c3a62c827d76e65dfa

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.15-alpha1742998768',
+      'ver' => '4.0.15',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
