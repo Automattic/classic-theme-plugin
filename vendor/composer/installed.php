c1b096ec88ca4a4ca01d4942167ca06d9612ef8c
anomiex <anomiex@users.noreply.github.com>
2025-04-10 15:03:55 +0000

    typescript: Use `rewriteRelativeImportExtensions` (#42990)
    TypeScript used to require that imports of `.ts` files use a `.js`
    extension, since that's what `tsc` would output. We have hacks in our
    webpack and jest configs to account for this.
    
    TypeScript 5.7 added a new option, `rewriteRelativeImportExtensions`,
    which will rewrite extensions in relative imports (i.e. imports that
    begin with `./` or `../`).
    
    Let's switch to that option and remove the hacks. Specifically:
    
    * Set `rewriteRelativeImportExtensions` true in
      `tools/js-tools/jest/jest-resolver.js`.
    * Remove `extensionAliases` in js-packages/webpack-config.
    * Remove `extensionAliases` in js-packages/storybook.
    * Remove logic from `tools/js-tools/jest/jest-resolver.js`
    * Update extensions in imports in JS and TS files all over the monorepo.
      And one in a webpack config.
    * Fix a handful of path errors that turned up when fixing those imports.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14383446673
    
    Upstream-Ref: Automattic/jetpack@40365de9c706f8f47413ba23a69153546723f920

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -22,7 +22,7 @@
         'automattic/jetpack-a8c-mc-stats' => array(
             'pretty_version' => '3.0.4',
             'version' => '3.0.4.0',
-            'reference' => '60f6fbb1f5aa1535536c23abaf00536c7441affc',
+            'reference' => 'a6d6b269a3c37631d428bc14f93b6d144e5dc8b7',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-a8c-mc-stats',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '4.0.18-alpha.1744139422',
             'version' => '4.0.18.0-alpha1744139422',
-            'reference' => 'cccd0b017ad2262869281489e71cbe5a5f360b98',
+            'reference' => '5d51d4dd868e879a8ada5379d28d95622feec167',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '5.0.6',
             'version' => '5.0.6.0',
-            'reference' => 'a98dc297939a443d0f9e057fe8b86d5a550e14d3',
+            'reference' => 'a28f59920fec1f64113a0f0f87a2edc4e9aca9fe',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/jetpack-classic-theme-helper' => array(
             'pretty_version' => '0.11.8-alpha.1744203531',
             'version' => '0.11.8.0-alpha1744203531',
-            'reference' => '2cd1c59ecbdfab474760cf9fce98db550f992249',
+            'reference' => 'ff3f81cde31628546367b3b474c7a0c4f986ec4f',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-classic-theme-helper',
             'aliases' => array(),
@@ -58,7 +58,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '4.0.4',
             'version' => '4.0.4.0',
-            'reference' => '5ab925263e8b3b12f7c4f6fd72533a34fae2767d',
+            'reference' => '775b991a1bef5203fe5b6991379d222520a2c0f7',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -67,7 +67,7 @@
         'automattic/jetpack-config' => array(
             'pretty_version' => '3.1.0',
             'version' => '3.1.0.0',
-            'reference' => '647cca76f8a2ac7db63b87a099366c274d3766ea',
+            'reference' => 'c0783315ae37665258677d7d86fc6b8c5edbf58b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-config',
             'aliases' => array(),
@@ -76,7 +76,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '3.0.7-alpha.1744139422',
             'version' => '3.0.7.0-alpha1744139422',
-            'reference' => '914eb81c53de0589f71c23a432a20cd6135e388c',
+            'reference' => '680d39c0fbde848fe03c7746e5f50c1862baed13',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
@@ -85,7 +85,7 @@
         'automattic/jetpack-plugins-installer' => array(
             'pretty_version' => '0.5.4',
             'version' => '0.5.4.0',
-            'reference' => '3f5747ac1096a2bd26e6ef6cf18c4944cbd24589',
+            'reference' => 'f5025f3f9264f0811e02b48f8ca95bb786823fac',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-plugins-installer',
             'aliases' => array(),
@@ -94,7 +94,7 @@
         'automattic/jetpack-status' => array(
             'pretty_version' => '5.1.2-alpha.1744139422',
             'version' => '5.1.2.0-alpha1744139422',
-            'reference' => 'ddf897dfa4a67060d5fd577a98b005e1b943a7ca',
+            'reference' => '3b4cb2848a5b2307e6079a2eb825296c1f4fc71d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-status',
             'aliases' => array(),
