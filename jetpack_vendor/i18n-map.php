39a4302fd6b7b6d17740547d4d75178956a429ab
tbradsha <tbradsha@users.noreply.github.com>
2025-06-19 15:55:38 +0000

    Update wordpress monorepo (#44020)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15761618317
    
    Upstream-Ref: Automattic/jetpack@75353b13ad6e5645b176b8a80e8385d74e39a9c8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.27',
+      'ver' => '4.0.28-alpha1750347518',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.13.7-alpha1750260074',
+      'ver' => '0.13.7-alpha1750347518',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
