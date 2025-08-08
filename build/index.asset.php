04d3f2960c3b8424274e023efb0793f62f1a1c4e
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-08-08 04:31:05 +0000

    Update @wordpress/eslint-plugin to 22.14.0 (#44686)
    * Bump @wordpress/eslint-plugin to 22.14.0
    
    * Fix stricter rules from @wordpress/i18n-translator-comments
    
    * Add changelogs
    
    * Update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16821752744
    
    Upstream-Ref: Automattic/jetpack@3dae8d11de4e58d6a7415290a45c6a1c1ce9c146

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bf5b82e573f4a9acd151');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '59c2116925cb823de48c');
