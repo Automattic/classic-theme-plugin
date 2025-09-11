1940eb293f1f1f23dec58fe48c63695c28694694
tbradsha <tbradsha@users.noreply.github.com>
2025-09-11 20:37:48 +0000

    Clean up various common typos in the monorepo (#45148)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17656336763
    
    Upstream-Ref: Automattic/jetpack@92b9b35201a27203cbbc7109921d81376cd5a6d8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.13.18-alpha1757538765',
+      'ver' => '0.13.18-alpha1757622058',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
