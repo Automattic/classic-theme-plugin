3f33393cd97004709266bd273cf610dca98251e2
kraftbj <kraftbj@users.noreply.github.com>
2025-10-28 16:26:51 +0000

    Boost Backport: Changelog and readme.txt edits. (#45664)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18881386963
    
    Upstream-Ref: Automattic/jetpack@516e58f6381d8664c89e6b299abfd320121d08c4

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.10',
+      'ver' => '4.3.11',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.0',
+      'ver' => '0.14.1-alpha1761667798',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
