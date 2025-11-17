88ea4ef00c15e0393d1511162524f2750f27f1b0
anomiex <anomiex@users.noreply.github.com>
2025-11-17 16:48:56 +0000

    Update wordpress monorepo (#45915)
    * Update wordpress monorepo
    * pnpmfile: `@automattic/components` is not compatible with `@wordpress/icons` v11
    * Pnpmfile hack for `@wordpress/media-utils` and `@wordpress/admin-ui` missing deps on `react`
    * tests: Fix deep mocking
      Various packages are now using `exports`, which means we can't have jest
      mock some deep entry point anymore. Fortunately all the tests seem ok
      with using a Proxy object to mock the whole package.
    * pnpm: Hack packages to flag ESM files correctly as ESM
      We do this by injecting a stub `package.json` into the directory holding
      the ESM `.js` files, because the resolution algorithm looks at the
      closest package.json rather than the package-as-a-whole's package.json.
    
      On the down side, we have to make sure to copy the `sideEffects`
      property into the stub, because that too looks at the closest one.
      Hopefully there aren't more things like that.
    
      In addition to the Gutenberg packages, the `uuid` package also has this
      issue showing up now for some reason.
    * webpack: Set `resolve.fullySpecified` to false for Gutenberg packages
      When the file is correctly flagged as containing ESM, Webpack by default
      requires the full file import rather than a directory or extensionless
      import. We can change that behavior by setting `resolve.fullySpecified`.
    
      Gutenberg itself doesn't run into this because they're relying on
      webpack/babel to be lenient with files that are flagged as CommonJS but
      contain ESM code.
    * scan: Revert dataviews test hack from #45551
      No longer necessary after the ESM-fixing hack in this PR.
    * Gutenberg seems to have stopped publishing the `.native.js` entry
      points. Disable linting of our `.native.[jt]s` files until that is
      resolved.
    * Fix typechecks
      We can't deep-import the types directly anymore, so we have to start
      with what we can import and use utility functions and accessors to drill
      down to the types we need.
    * forms: Hack around `@wordpress/interactivity` going ESM-only.
      I worked out a hacky way to get the test to pass without being too
      invasive.
    
      In the long run it would be better to convert `packages/forms` to run
      Jest in ESM mode, i.e.
      `NODE_OPTIONS=--experimental-vm-modules jest --config=tests/jest.config.js`.
      But making that happen will be a lot more invasive: probably you'll need
      to set `"type": "module"` in `package.json` too so all the existing
      ESM-containing `.js` files get properly interpreted as ESM, and then
      you'll have to fully specify all the imports (i.e. change `import from
      './subdir'` to `import from './subdir/index.js'` and `import from
      './file'` to `import from './file.js'`), and clean up anything else that
      breaks too.
    
      On the easier side, you could just rename some of the config files, like
      `babel.config.js` → `babel.config.cjs` and `tools/webpack.config.*.js` →
      `tools/webpack.config.*.cjs`, if converting them is too annoying.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19436971085
    
    Upstream-Ref: Automattic/jetpack@9bf43b421c2aa4379fac245d08d00981091fcee9

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.12',
+      'ver' => '4.3.13-alpha1763397196',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.2-alpha1763047798',
+      'ver' => '0.14.2-alpha1763397196',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
