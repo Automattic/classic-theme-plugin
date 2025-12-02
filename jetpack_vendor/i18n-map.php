b028a6eb4b7824812d2d5ca1530ef573ebdd0cff
anomiex <anomiex@users.noreply.github.com>
2025-12-02 14:47:46 +0000

    Backport jetpack 15.3-beta changes (#46157)
    * Changelog and readme.txt edits.
    * Amend readme.txt
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Grzegorz Chudzinski-Pawlowski <>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19862089357
    
    Upstream-Ref: Automattic/jetpack@fe9328a088999d9b35a38975723313959f7cb417

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.14-alpha1764612022',
+      'ver' => '4.3.14',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.4-alpha1764612022',
+      'ver' => '0.14.4',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
