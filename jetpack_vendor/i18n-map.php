1b32276361fd19d33910f4c71385968bc0d8d681
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-12-09 13:53:20 +0000

    Backport jetpack 15.4-a.1 changes (#46224)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Fix changelog message for social-logos
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20065387693
    
    Upstream-Ref: Automattic/jetpack@c0e09a81258c7acdfdeb74573eb82a287fd6d3d1

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.15-alpha1764773158',
+      'ver' => '4.3.15',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.5-alpha1764773158',
+      'ver' => '0.14.5',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
