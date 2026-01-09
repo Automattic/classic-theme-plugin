b33b6bf489e2e8457c6b04415ca346bc18716098
anomiex <anomiex@users.noreply.github.com>
2026-01-09 21:04:55 +0000

    Backport mu-wpcom-plugin 2.12.0 changes (#46515)
    * Changelog and readme.txt edits.
    
    ---------
    
    Co-authored-by: Grzegorz Chudzinski-Pawlowski <>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20865093177
    
    Upstream-Ref: Automattic/jetpack@f7a42aa02a8cc6ed5ad50132c04b591b3bbc8085

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -59,39 +59,39 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-plugins-installer/src/class-automatic-install-skin.php'
 	),
 	'Automattic\\Jetpack\\Blocks' => array(
-		'version' => '3.2.0.0-alpha1767973458',
+		'version' => '3.3.0.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-blocks/src/class-blocks.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Featured_Content' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/class-featured-content.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Jetpack_Portfolio' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/custom-post-types/class-jetpack-portfolio.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Jetpack_Testimonial' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/custom-post-types/class-jetpack-testimonial.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Jetpack_Testimonial_Textarea_Control' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/custom-post-types/class-jetpack-testimonial-textarea-control.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Jetpack_Testimonial_Title_Control' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/custom-post-types/class-jetpack-testimonial-title-control.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Main' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/class-main.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Nova_Restaurant' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/custom-post-types/class-nova-restaurant.php'
 	),
 	'Automattic\\Jetpack\\Classic_Theme_Helper\\Social_Links' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/class-social-links.php'
 	),
 	'Automattic\\Jetpack\\Composer\\Manager' => array(
@@ -167,7 +167,7 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/class-hook-manager.php'
 	),
 	'Jetpack_Customize_Control_Title' => array(
-		'version' => '0.14.8.0-alpha1767991196',
+		'version' => '0.14.9.0-alpha1767991613',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-classic-theme-helper/src/content-options/customizer.php'
 	),
 	'Latest_Autoloader_Guard' => array(
