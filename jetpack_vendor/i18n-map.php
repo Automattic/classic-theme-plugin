115d2e96d6e1dd739c9379f9375980f9ef560c3c
anomiex <anomiex@users.noreply.github.com>
2026-01-30 18:08:30 +0000

    Update wordpress monorepo (#46854)
    * Update wordpress monorepo
    * Charts: Fix TypeScript error in story by using spacing instead of gap on HStack.
    * Components: Fix TypeScript errors with WordPress components type changes
    * Cast modal title to string in ThreatModal
    * Add label to threats status toggle group
    * Remove unused component
    * Fix type errors related to ExternalLink missing children
    * Fix icon type in forms actions
    * Fix the remaining TS errors in forms
    * NavigableMenu passes stopNavigationEvents by default
    * Use controlled value for Filename TextControl
    * Fix errors in my jetpack
    * Fix publicize errors except tab panel one to avoid conflicts with #46856
    * Fix multiple errors in jetpack blocks
    * Suppress errors in the remaining publicize components for now
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21525260613
    
    Upstream-Ref: Automattic/jetpack@ab92a74db764763e0805d1a2995a215d3399040c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.22-alpha1769720062',
+      'ver' => '4.3.22-alpha1769795377',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.12-alpha1769616682',
+      'ver' => '0.14.12-alpha1769795377',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
