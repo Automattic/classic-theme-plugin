047c07aafd0f1bb2f6b8bcbe3c135ab5026e74b0
anomiex <anomiex@users.noreply.github.com>
2026-02-09 16:00:13 +0000

    Update dependency webpack to v5.104.1 [SECURITY] (#47002)
    * Update dependency webpack to v5.104.1 [SECURITY]
    * i18n-check-webpack-plugin: Update snapshots
    * i18n-loader-webpack-plugin: Fix test and update snapshots
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21831619410
    
    Upstream-Ref: Automattic/jetpack@ff8455f839c27f8b7797eaaae15af73321e30464

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.22',
+      'ver' => '4.3.23-alpha1770651640',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.13-alpha1770153807',
+      'ver' => '0.14.13-alpha1770651640',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
