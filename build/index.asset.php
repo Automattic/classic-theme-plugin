4344fd5527f9e3c0bfa20b4c00cb21b0d65331e4
tbradsha <tbradsha@users.noreply.github.com>
2026-04-17 13:01:30 +0000

    Update wordpress monorepo (#48106)
    * Fix `@wordpress/ui` peer dep issue.
    * Charts: Rename WPDS font tokens to typography-* for @wordpress/theme@0.11.0
    * Fix TS errors
    * Clean up global types mess in mu-wpcom
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24565711396
    
    Upstream-Ref: Automattic/jetpack@5cca8ab3cec81465ff7fcc8d169be4983f83d7c6

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '907ee7c622579d181796');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '485ed2beabd0fad31fa5');
