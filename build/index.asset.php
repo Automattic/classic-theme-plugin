84230a9ce20850259860f47f91d4f5880ebc159e
anomiex <anomiex@users.noreply.github.com>
2026-05-11 17:45:16 +0000

    Lock file maintenance (#48683)
    * Lock file maintenance
    * Downgrade `@tanstack/react-router`
      https://href.li/?https://github.com/TanStack/router/issues/7176 wasn't fixed after all.
    * Downgrade `@wordpress/patterns`
      v2.45.0 conflicts with `@wordpress/fields` v0.36.0.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25686166415
    
    Upstream-Ref: Automattic/jetpack@803fc4793c413ffcbc5e8711044fee5ea6008c91

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'f5cc2a28e42abab4db21');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'b456cc412088813816eb');
