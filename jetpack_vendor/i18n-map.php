e2a7996bc945a75324f6e52e2e160971e5bcba20
anomiex <anomiex@users.noreply.github.com>
2026-05-14 18:34:27 +0000

    Clean up boilerplate `.gitattributes` (#48802)
    Add a lot of generic tooling `production-excludes` entries to the
    monorepo root `.gitattributes`, and remove them from project
    `.gitattributes`.
    
    Also, have the mirror build always generate a `.gitattributes` that sets
    `export-ignore` for `.git*` files, and additionally strip the setting of
    any `production-include` and `production-exclude` attributes in the
    mirror copy.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25877340776
    
    Upstream-Ref: Automattic/jetpack@bf1983deeacdfdea13fdfb490eae62222967b6fc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,39 +6,39 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.4',
+      'ver' => '0.8.5-alpha1778782620',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.36',
+      'ver' => '4.3.37-alpha1778782620',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
-      'ver' => '0.3.6',
+      'ver' => '0.3.7-alpha1778782620',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.27-alpha1778670790',
+      'ver' => '0.14.27-alpha1778782620',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '3.1.2',
+      'ver' => '3.1.3-alpha1778782620',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.3.5',
+      'ver' => '8.3.6-alpha1778782620',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.25',
+      'ver' => '0.7.26-alpha1778782620',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.8',
+      'ver' => '0.5.9-alpha1778782620',
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
-      'ver' => '0.1.2',
+      'ver' => '0.1.3-alpha1778782620',
     ),
   ),
 );
