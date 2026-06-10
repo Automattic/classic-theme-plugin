72f778b4a91eb3b8f52f90ad2472dcbdf71a22bc
tbradsha <tbradsha@users.noreply.github.com>
2026-06-10 17:24:01 +0000

    PHPCS: Remove unused suppressions (#49443)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27292580137
    
    Upstream-Ref: Automattic/jetpack@dc72cffc5c1afb5b38a4494bd8715300346f4e98

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.14.31-alpha1781005495',
+      'ver' => '0.14.31-alpha1781111196',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.6.0',
+      'ver' => '8.6.1-alpha1781111196',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.28',
+      'ver' => '0.7.29-alpha1781111196',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-post-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-media',
-      'ver' => '0.1.5',
+      'ver' => '0.1.6-alpha1781111196',
     ),
   ),
 );
