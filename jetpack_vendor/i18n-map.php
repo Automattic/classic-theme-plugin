5523ed6b6dff8a9b8316f233a3a48a1a81e0521f
anomiex <anomiex@users.noreply.github.com>
2026-07-13 17:00:26 +0000

    Lock file maintenance (#50436)
    * Lock file maintenance
    * Fix new WPCS whitespace sniffs
    * Downgrade `@tanstack/react-router` to 1.167.1
      Avoid https://href.li/?https://github.com/TanStack/router/issues/7538
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29267506434
    
    Upstream-Ref: Automattic/jetpack@4447decd63f96040eb2350955798da6e4d2fa59d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.7',
+      'ver' => '8.7.8-alpha1783960952',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
