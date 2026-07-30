f8390ca325c04e4adbe2f4eaa9aee5dd56448f3c
dmallory42 <dmallory42@users.noreply.github.com>
2026-07-30 07:20:15 +0000

    Connection: cache wpcom.getUser XML-RPC failures in get_connected_user_data() (#50230)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30521092388
    
    Upstream-Ref: Automattic/jetpack@ade93b174cc595c8fc183cc6b6435ff883cd6b9c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.8.1-alpha1785246187',
+      'ver' => '8.8.1-alpha1785394465',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
