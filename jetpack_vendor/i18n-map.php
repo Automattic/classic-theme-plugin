1b63db026e994e3dc648337fa6e39945a3a30831
enejb <enejb@users.noreply.github.com>
2026-08-26 21:40:37 +0000

    My Jetpack: allow the Automattic for Agencies banner to be dismissed (#51441)
    * My Jetpack: allow the Automattic for Agencies banner to be dismissed
    
    * My Jetpack: use the components package Button for the banner dismiss control
    
    * My Jetpack: document why the A4A banner dismissal is stored per site
    
    * My Jetpack: cache the partner lookup when a site has no partner
    
    * My Jetpack: treat a forbidden partner lookup as a cacheable answer
    
    * My Jetpack: keep the A4A banner dismissed when returning to the Overview tab
    
    * My Jetpack: tidy the A4A dismissal internals and make the partner tests order-independent
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33014809457
    
    Upstream-Ref: Automattic/jetpack@63d9d17800d1db3faab98c9363231287002a0ea3

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.0.1-alpha1787777208',
+      'ver' => '9.0.1-alpha1787779161',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
