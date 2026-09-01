13a3b70647fe4992a2c22e4003cf8c301967c4f9
anomiex <anomiex@users.noreply.github.com>
2026-09-01 13:59:52 +0000

    Update definitelyTyped (#51802)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33514739269
    
    Upstream-Ref: Automattic/jetpack@ac008837d1bdba91b3f1c87e0392a6d9047b1fb5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.0-alpha1788268718',
+      'ver' => '9.1.0-alpha1788269986',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
