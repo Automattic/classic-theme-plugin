21d4cffa40904ec9e4ca04d4ecd532440753ea9a
kraftbj <kraftbj@users.noreply.github.com>
2026-09-08 18:37:08 +0000

    Status: recognize the whole 127.0.0.0/8 loopback range and 0.0.0.0 as local (#51311)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34261847743
    
    Upstream-Ref: Automattic/jetpack@c636aaedd0796249314e68081e1165fca0b1cf29

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -291,15 +291,15 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-constants/src/class-constants.php'
 	),
 	'Automattic\\Jetpack\\CookieState' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-cookiestate.php'
 	),
 	'Automattic\\Jetpack\\Errors' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-errors.php'
 	),
 	'Automattic\\Jetpack\\Files' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-files.php'
 	),
 	'Automattic\\Jetpack\\Heartbeat' => array(
@@ -351,7 +351,7 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-image-cdn/src/class-image-cdn-setup.php'
 	),
 	'Automattic\\Jetpack\\Modules' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-modules.php'
 	),
 	'Automattic\\Jetpack\\Partner' => array(
@@ -363,7 +363,7 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-partner-coupon.php'
 	),
 	'Automattic\\Jetpack\\Paths' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-paths.php'
 	),
 	'Automattic\\Jetpack\\Plugins_Installer' => array(
@@ -395,23 +395,23 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-post-media/src/class-shortcodes.php'
 	),
 	'Automattic\\Jetpack\\Status' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-status.php'
 	),
 	'Automattic\\Jetpack\\Status\\Cache' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-cache.php'
 	),
 	'Automattic\\Jetpack\\Status\\Host' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-host.php'
 	),
 	'Automattic\\Jetpack\\Status\\Request' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-request.php'
 	),
 	'Automattic\\Jetpack\\Status\\Visitor' => array(
-		'version' => '7.0.0.0',
+		'version' => '7.0.1.0-alpha1788891260',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-visitor.php'
 	),
 	'Automattic\\Jetpack\\Terms_Of_Service' => array(
