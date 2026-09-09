53d72b402e1373e3e5276edab4b9f6617e808b75
coder-karen <coder-karen@users.noreply.github.com>
2026-09-09 09:07:42 +0000

    Connection: Implement shared connection error notice (#51504)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34331035237
    
    Upstream-Ref: Automattic/jetpack@a076ca15bf3a105bc44e36ccd4180873bf66ccd0

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.1',
+      'ver' => '9.1.2-alpha1788943628',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
