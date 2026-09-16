a5ca3ec63c09f20ceca223453f1417ee85c890ef
enejb <enejb@users.noreply.github.com>
2026-09-16 19:59:50 +0000

    My Jetpack: add a flag that replaces the Products tab with a Features tab (#52346)
    * My Jetpack: add a flag that replaces the Products tab with a Features tab
    
    * My Jetpack: publish the products tab slug and label once for the tab, links and footers
    
    * My Jetpack: keep trunk's Products strings while the Features tab is off, and cover the review gaps
    
    * My Jetpack: address review on the Features tab flag
    
    * Jetpack: move the footer products link helper out of Jetpack_Admin_Page
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35141565627
    
    Upstream-Ref: Automattic/jetpack@34903b0292964820f57d1bbe844f9a5873d29883

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '065beef634bff2cb2e71');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cae11d372d0ea77bf833');
