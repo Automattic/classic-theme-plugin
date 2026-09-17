43eaac257addbe106dc7b15b03d20cfa25b14f2e
anomiex <anomiex@users.noreply.github.com>
2026-09-17 19:21:06 +0000

    Update dependency prettier to v3.9.6 (#52356)
    * Update dependency prettier to v3.9.6
    * Fix code in `.github/renovate-config.js` that prettier now breaks.
    * Configure to not add trailing commas to `.jsonc` files.
    * Run `pnpm lint-required --fix` to reformat hundreds of files.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35262202647
    
    Upstream-Ref: Automattic/jetpack@5090b017f1d670095ed6cf41d34feb15aedb20fa

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.3.1-alpha1789670548',
+      'ver' => '9.3.1-alpha1789671588',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
