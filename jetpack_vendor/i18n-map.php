d3271e492fda43ec0852c7178891f302b50ec028
haqadn <haqadn@users.noreply.github.com>
2026-09-17 19:05:17 +0000

    Backport jetpack 16.3-a.1 changes (#52349)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Update projects/packages/forms/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/connection/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/plugins/jetpack/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Update projects/packages/paypal-payments/CHANGELOG.md
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35260462577
    
    Upstream-Ref: Automattic/jetpack@5475aa3314d6cda0306c99c4f6bff00636df6950

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.12.0-alpha1789559411',
+      'ver' => '0.12.1-alpha1789670548',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.4-alpha1789575972',
+      'ver' => '5.0.5-alpha1789670548',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.15.2-alpha1789559411',
+      'ver' => '0.15.3-alpha1789670548',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.3.0-alpha1789576905',
+      'ver' => '9.3.1-alpha1789670548',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
