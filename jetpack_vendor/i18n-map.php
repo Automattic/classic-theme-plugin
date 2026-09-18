4941a9d0f931215454a9d18873b773954eff8b59
heydemoura <heydemoura@users.noreply.github.com>
2026-09-18 19:43:22 +0000

    Backport videopress 3.5 changes (#52501)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Heyde Moura <heyde.moura@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35385491543
    
    Upstream-Ref: Automattic/jetpack@f4ecfbd29b20ffe0ec30c5502439997c784b125a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,11 +6,11 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.12.1-alpha1789758629',
+      'ver' => '0.12.2-alpha1789759296',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.5-alpha1789670548',
+      'ver' => '5.0.5',
     ),
     'jetpack-block-delimiter' => array(
       'path' => 'jetpack_vendor/automattic/block-delimiter',
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.4.0-alpha1789738120',
+      'ver' => '9.4.0',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
