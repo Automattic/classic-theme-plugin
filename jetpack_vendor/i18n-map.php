84da124d9f062e9a9d1589bb2bc693f620e10f76
dsas <dsas@users.noreply.github.com>
2026-09-18 09:05:54 +0000

    Custom Colors: Fix translucent color rules output as transparent (#52433)
    css_rule() passed intval( $number ) to Jetpack_Color::toCSS() for
    opacity rules, so an alpha such as 0.2 became 0 and the rule rendered
    as rgba( r, g, b, 0 ). The intval() was added in a Phan pass because
    toCSS() documented $alpha as an int; correct that docblock to float.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35325861846
    
    Upstream-Ref: Automattic/jetpack@9df401902d5338cd20dd1d7a2cb14c547bd500c9

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.15.3-alpha1789670548',
+      'ver' => '0.15.3-alpha1789721031',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
