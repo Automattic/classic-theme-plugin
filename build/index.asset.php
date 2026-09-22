14bceb3fd221b19d81abf9446377c620bba93631
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-09-22 08:17:51 +0000

    Boost: Hide footer links when My Jetpack is unavailable (#52557)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35701844866
    
    Upstream-Ref: Automattic/jetpack@0ea5f95bfe601eb0135c14391160b8684fc2a54f

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5395a0284832f8c6972b');
+<?php return array('dependencies' => array('jetpack-connection', 'jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cf4e633bd75b882ba9b5');
